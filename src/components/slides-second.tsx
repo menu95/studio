3ae62ec3a33c4c33b1992d6c0bcace8a082fc372
menu95 <100180464+menu95@users.noreply.github.com>
2026-02-11 19:23:07 -0300

    Update slides-second.tsx

--- src/components/slides-second.tsx
+++ src/components/slides-second.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react'; 
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'; 
 import {
     HandCoins, CalendarDays, Target, Activity, HeartHandshake,
     Megaphone, LayoutDashboard, FileText, RefreshCw, Star,
